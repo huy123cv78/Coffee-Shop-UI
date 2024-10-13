f483fde0d654cb07dd43949196b0751c5366242b
huy123cv78 <huy123cv78@gmail.com>
2024-10-13 11:41:12 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/welcome_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF212325),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
